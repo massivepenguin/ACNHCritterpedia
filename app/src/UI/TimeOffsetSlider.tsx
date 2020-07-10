db2dba351ee919e022461b7688a1ca6ec55e6dc4
Michael Armstrong <v-armich@microsoft.com>
2020-07-10 16:02:50 +0100

    tweak naming and code formatting

--- app/src/UI/TimeOffsetSlider.tsx
+++ app/src/UI/TimeOffsetSlider.tsx
@@ -10,18 +10,30 @@ function TimeOffsetSlider() {
     let sliderVal = state.timeOffset;
 
     const setValue = (e: React.SyntheticEvent<HTMLInputElement>) => {
+        // set the app's timeOffset property only when interaction is complete to prevent re-renders on inout movement
         store.dispatch(changeOffset(parseInt(e.currentTarget.value, 10)));
     }
 
-    const updateValue = (e: React.SyntheticEvent<HTMLInputElement>) => {
-        setTextVal(e.currentTarget.value);
+    const updateTextValue = (e: React.SyntheticEvent<HTMLInputElement>) => {
+        // set the component's state value, not the app's state
+        setTextVal(e.currentTarget.value.toString());
     }
 
     return (
         <div className={'offset-slider'}>
             <label>Timezone Offset</label>
             <div>{textVal}</div>
-            <input type={'range'} min={-12} max={12} step={0.5} defaultValue={sliderVal} id={'offset'} onInput={updateValue} onMouseUp={setValue} onTouchEnd={setValue} onDrop={setValue} />
+            <input 
+                type={'range'} 
+                min={-12} 
+                max={12} 
+                step={0.5} 
+                defaultValue={sliderVal} 
+                id={'offset'} 
+                onInput={updateTextValue} 
+                onMouseUp={setValue} 
+                onTouchEnd={setValue}
+            />
         </div>
     );
 }
