42b633a7f4a573b9ea7b1a3a102add398e13c67e
Michael Armstrong <v-armich@microsoft.com>
2020-07-23 10:56:44 +0100

    update test for more conditions

--- app/src/__tests__/CritterFilters.test.ts
+++ app/src/__tests__/CritterFilters.test.ts
@@ -8,6 +8,10 @@ it('sorting test', () => {
     const critterSet2: ICritterState = {caught: {bugs: [10, 75], fish: [], seaCreatures: []}, donated: {bugs: [], fish: [], seaCreatures: []}}
     const northResultAll = filterCritters(0, hemisphere.north, critterSet1, false, sortType.alphaAsc, new Date(2020, 6, 16, 10)); // 16th July 2020 10:00:00
     const northResultCaughtHidden = filterCritters(0, hemisphere.north, critterSet2, true, sortType.entryAsc, new Date(2020, 6, 16, 10)); // 16th July 2020 10:00:00
+    const southResultAll = filterCritters(0, hemisphere.south, critterSet1, false, sortType.alphaAsc, new Date(2020, 6, 16, 10)); // 16th July 2020 10:00:00
     expect(northResultAll.available.bugs.length).toEqual(39);
     expect(northResultCaughtHidden.available.bugs.length).toEqual(37);
+    expect(southResultAll.available.bugs.length).toEqual(15);
+    expect(southResultAll.available.fish.length).toEqual(24);
+    expect(southResultAll.available.seaCreatures.length).toEqual(17);
 });
