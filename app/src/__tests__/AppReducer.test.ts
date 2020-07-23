ea76f03fee0e64ce91af0b92d0a1fefa8e28dacf
Michael Armstrong <v-armich@microsoft.com>
2020-07-23 11:26:56 +0100

    update tests

--- app/src/__tests__/AppReducer.test.ts
+++ app/src/__tests__/AppReducer.test.ts
@@ -1,5 +1,9 @@
-import { testStore, catchCritter, donateCritter } from "../reducers/AppReducer";
+import { testStore, catchCritter, donateCritter, updateState } from "../reducers/AppReducer";
 import { critterType } from "../model/CritterType";
+import { instanceOfAppState } from "../model/AppState";
+import { mainAppView } from "../model/MainAppView";
+import { sortType } from "../model/SortType";
+import { ICritterIdList } from "../model/ICritterIdList";
 
 it('catch bug with ID of 10', () => {
     const store = testStore;
@@ -36,4 +40,32 @@ it('donate a bug with ID of 10, then undonate', () => {
     const state = store.getState();
     expect(state.critters.caught.bugs).toContain(critterId);
     expect(state.critters.donated.bugs).not.toContain(critterId);
+});
+
+it('update out-of-date state to current app state', () => {
+    const outOfDateState = {
+        currentView: mainAppView.all,
+        critters: {
+            caught: { bugs: [1,5,7], fish: [8,25,67,48]  } as any, // missing seaCreatures
+            donated: { bugs: [5], fish: [8,25] } as any, // missing seaCreatures
+        },
+        activeSort: sortType.entryAsc,
+        timeOffset: 0,
+        hemisphere: null,
+        hideCaught: false,
+        showAll: false,
+    };
+    expect(instanceOfAppState(outOfDateState)).toEqual(false);
+    const updatedState = updateState(outOfDateState);
+    expect(instanceOfAppState(updatedState)).toEqual(true);
+    expect(updatedState.critters.caught.bugs.length).toEqual(3);
+    expect(updatedState.critters.caught.bugs).toContain(5);
+    expect(updatedState.critters.caught.fish.length).toEqual(4);
+    expect(updatedState.critters.caught.fish).toContain(67);
+    expect(updatedState.critters.donated.bugs.length).toEqual(1);
+    expect(updatedState.critters.donated.bugs).toContain(5);
+    expect(updatedState.critters.donated.fish.length).toEqual(2);
+    expect(updatedState.critters.donated.fish).toContain(25);
+    expect(updatedState.critters.caught.seaCreatures.length).toEqual(0);
+    expect(updatedState.critters.donated.seaCreatures.length).toEqual(0);
 });
